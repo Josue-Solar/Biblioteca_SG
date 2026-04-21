c2789ec2a0db0b5a2c946cf2babeb577b17655fe
rodrigo-vives <rod.vives@duocuc.cl>
2026-04-21 12:20:20 -0400

    Update Persona.java

--- Persona/persona/src/main/java/com/biblioteca/persona/model/Persona.java
+++ Persona/persona/src/main/java/com/biblioteca/persona/model/Persona.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Persona {
+    //comentario
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
