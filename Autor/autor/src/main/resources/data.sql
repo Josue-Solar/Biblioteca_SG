711da9a909e908c29a82b95a15960dd161bb6bd3
FranT-prog <f.torres5@duocuc.cl>
2026-06-25 12:38:40 -0400

    Se agregó import sql en Autor
    Se agregó como data.sql en Autor

---
+++ Autor/autor/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- data.sql
+
+INSERT INTO autor (pnombre, snombre, apellido_paterno, apellido_materno) 
+VALUES ('Gabriel', 'José', 'García', 'Márquez');
+
+INSERT INTO autor (pnombre, snombre, apellido_paterno, apellido_materno) 
+VALUES ('Isabel', NULL, 'Allende', 'Llona');
+
+INSERT INTO autor (pnombre, snombre, apellido_paterno, apellido_materno) 
+VALUES ('Jorge', 'Luis', 'Borges', NULL);
+
+INSERT INTO autor (pnombre, snombre, apellido_paterno, apellido_materno) 
+VALUES ('Julio', NULL, 'Cortázar', NULL);
